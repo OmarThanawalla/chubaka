3204d1188f65f03314839f1263d3fb6560a343d0
Slavcho Slavchev <slavcho92@gmail.com>
2012-11-02 23:12:17 -0700

    'Initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chubaka::Application.config.secret_token = 'd63ec6c3ff134136d8b5efbc755b34472d95a0eb1e2eccb026c82962cfe7330f435474e323d74411a8e1c908ac05cfe21c8787797543c72b5fd89d1ab02c7dff'
